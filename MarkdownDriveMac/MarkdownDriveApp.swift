a49c42af490181d8dc196e6cb3f980ce05779b9a
a-know <a.know.dev@gmail.com>
2026-08-13 22:25:26 +0900

    feat: protect unsaved edits on quit

--- MarkdownDriveMac/MarkdownDriveApp.swift
+++ MarkdownDriveMac/MarkdownDriveApp.swift
@@ -1,7 +1,9 @@
+import AppKit
 import SwiftUI
 
 @main
 struct MarkdownDriveApp: App {
+    @NSApplicationDelegateAdaptor(AppTerminationDelegate.self) private var appDelegate
     @StateObject private var appModel: AppModel
 
     init() {
@@ -13,6 +15,9 @@ struct MarkdownDriveApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView(appModel: appModel)
+                .onAppear {
+                    appDelegate.appModel = appModel
+                }
                 .task {
                     await appModel.restoreSession()
                 }
@@ -21,6 +26,63 @@ struct MarkdownDriveApp: App {
         .commands {
             MarkdownEditorCommands()
             MarkdownDocumentCommands(appModel: appModel)
+        }
+    }
+}
+
+@MainActor
+private final class AppTerminationDelegate: NSObject, NSApplicationDelegate {
+    weak var appModel: AppModel?
+
+    private var isTerminationPending = false
+
+    func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
+        guard let appModel else {
+            return .terminateNow
+        }
+        guard appModel.hasDirtyDocument else {
+            return .terminateNow
+        }
+        guard appModel.documentSaveState != .saving else {
+            let alert = NSAlert()
+            alert.alertStyle = .warning
+            alert.messageText = "A Save Is Still in Progress"
+            alert.informativeText =
+                "Markdown Drive will stay open until you try to quit again after the save finishes."
+            alert.addButton(withTitle: "OK")
+            alert.runModal()
+            return .terminateCancel
+        }
+        guard !isTerminationPending else {
+            return .terminateLater
+        }
+
+        let alert = NSAlert()
+        alert.alertStyle = .warning
+        alert.messageText = "Do you want to save your changes before quitting?"
+        alert.informativeText =
+            "If you quit without saving, the changes in the current document will be lost."
+        let saveButton = alert.addButton(withTitle: "Save and Quit")
+        saveButton.keyEquivalent = "\r"
+        let cancelButton = alert.addButton(withTitle: "Cancel")
+        cancelButton.keyEquivalent = "\u{1b}"
+        let discardButton = alert.addButton(withTitle: "Quit Without Saving")
+        discardButton.hasDestructiveAction = true
+
+        switch alert.runModal() {
+        case .alertFirstButtonReturn:
+            isTerminationPending = true
+            Task { @MainActor [weak self, weak sender] in
+                await appModel.saveDocument()
+                let didSaveAllChanges = !appModel.hasDirtyDocument
+                self?.isTerminationPending = false
+                sender?.reply(toApplicationShouldTerminate: didSaveAllChanges)
+            }
+            return .terminateLater
+        case .alertThirdButtonReturn:
+            return .terminateNow
+        default:
+            return .terminateCancel
         }
     }
 }
